a51ccda90c3a2314683a3ae63d3e29431820978e
Jakub Parzonka <jakub.parzonka@outlook.com>
2017-12-15 02:44:35 -0800

    Initial commit. Creating socket.

--- main.c
+++ main.c
@@ -2,26 +2,48 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     system("clear");
-    if (argc != 3) {
-        puts("Invalid number of arguments");
+    if (argc != 3 ) {
+        puts("Give me more arguments!");
         exit(1);
     }
-    int sock_fd;
-    int port_nr;
+    int socketIsAwesome;
+    int portNumber;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     char buffer[256];
     int n;
 
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd < 0) {
-        perror("Problem during socket creation => ");
+    socketIsAwesome = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketIsAwesome < 0) perror("Problem during socket creation => ");
+
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        perror("getHostByName error => ");
+        exit(1);
     }
 
+    //setting things to 0
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
+    portNumber = atoi(argv[2]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons((uint16_t) portNumber);
 
+    if (connect(socketIsAwesome, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Problem during connecting to server => ");
+        exit(1);
+    } else {
+        puts("Connection established.");
+    }
+
+    while(1){
+        puts("Please ");
+    }
 
     return 0;
 }
